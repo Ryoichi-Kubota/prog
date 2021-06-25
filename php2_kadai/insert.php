65aca44be38a1e277246b04bf3b20ead5ba01eb0
Ryoichi-Kubota <81688850+Ryoichi-Kubota@users.noreply.github.com>
2021-06-26 06:49:41 +0900

    6回目授業課題

---
+++ php2_kadai/insert.php
@@ -0,0 +1,57 @@
+<?php
+//1. POSTデータ取得
+
+//まず前のphpからデーターを受け取る（この受け取ったデータをもとにbindValueと結びつけるため）
+$last_name = $_POST["last_name"];
+$age = $_POST["age"];
+$annual_income = $_POST["annual_income"];
+$device = $_POST["device"];
+$sales = $_POST["sales"];
+$rank = $_POST["rank"];
+$answer_kakaku = $_POST["answer_kakaku"];
+$answer_design = $_POST["answer_design"];
+$answer_support = $_POST["answer_support"];
+$answer_brand = $_POST["answer_brand"];
+$answer_quality = $_POST["answer_quality"];
+
+
+//2. DB接続します xxxにDB名を入力する
+//ここから作成したDBに接続をしてデータを登録します xxxxに作成したデータベース名を書きます
+// mamppの方は
+// $pdo = new PDO('mysql:dbname=xxx;charset=utf8;host=localhost', 'root', 'root');
+
+try {
+    $pdo = new PDO('mysql:dbname=analytics_db;charset=utf8;host=localhost', 'root', '');
+} catch (PDOException $e) {
+    exit('DbConnectError:'.$e->getMessage());
+}
+
+
+//３．データ登録SQL作成 //ここにカラム名を入力する
+//xxx_table(テーブル名)はテーブル名を入力します
+$stmt = $pdo->prepare("INSERT INTO a_table(id, last_name, indate, device, age, annual_income, rank, answer_kakaku, answer_design, answer_support, answer_brand, answer_quality, sales
+ )VALUES(NULL, :last_name, sysdate(), :device, :age, :annual_income, :rank, :answer_kakaku, :answer_design, :answer_support, :answer_brand, :answer_quality, :sales)");
+$stmt->bindValue(':last_name', $last_name, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':device', $device, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':age', $age, PDO::PARAM_INT);  //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':annual_income', $annual_income, PDO::PARAM_INT);  //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':rank', $rank, PDO::PARAM_INT);  //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':answer_kakaku', $answer_kakaku, PDO::PARAM_INT);  //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':answer_design', $answer_design, PDO::PARAM_INT);  //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':answer_support', $answer_support, PDO::PARAM_INT);  //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':answer_brand', $answer_brand, PDO::PARAM_INT);  //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':answer_quality', $answer_quality, PDO::PARAM_INT);  //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':sales', $sales, PDO::PARAM_INT);  //Integer（数値の場合 PDO::PARAM_INT)
+
+$status = $stmt->execute();
+
+//４．データ登録処理後
+if ($status==false) {
+    //SQL実行時にエラーがある場合（エラーオブジェクト取得して表示）
+    $error = $stmt->errorInfo();
+    exit("QueryError:".$error[2]);
+} else {
+    //５．index.phpへリダイレクト 書くときにLocation: in この:　のあとは半角スペースがいるので注意！！
+    header("Location: index.php");
+    exit;
+}
